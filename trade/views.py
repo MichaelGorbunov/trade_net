63fb74055d008f6dd28aa45fca23d2c64a510b01
Michael Gorbunov <mg0rby@yandex.ru>
2025-03-03 08:01:16 +0300

    debt_to_supplier не обновляется через api

--- trade/views.py
+++ trade/views.py
@@ -1,8 +1,11 @@
 from rest_framework import generics
+from rest_framework.response import Response
+
 from .models import TradeNode
 from .serializers import TradeNodeSerializer
 from django_filters.rest_framework import DjangoFilterBackend
 from .filters import TradeNodeFilter
+
 
 class TradeNodeListCreateView(generics.ListCreateAPIView):
     queryset = TradeNode.objects.all()
@@ -10,6 +13,19 @@ class TradeNodeListCreateView(generics.ListCreateAPIView):
     filter_backends = [DjangoFilterBackend]
     filterset_class = TradeNodeFilter
 
+
 class TradeNodeRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = TradeNode.objects.all()
     serializer_class = TradeNodeSerializer
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        data = request.data.copy()
+        if 'debt_to_supplier' in data:
+            del data['debt_to_supplier']
+        serializer = self.get_serializer(instance, data=data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        return Response(serializer.data)
